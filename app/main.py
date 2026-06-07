c867eec08f098eb79de335258ed47ea93322d58f
user <lahunouanton@gmail.com>
2026-06-07 14:53:35 +0200

    Commit to check workflow is working

--- app/main.py
+++ app/main.py
@@ -3,9 +3,7 @@ from app.api import router as birds_router
 from app.core.database import engine, Base
 
 Base.metadata.create_all(bind=engine)
-
 app = FastAPI(title="Bird Feeder API")
-
 app.include_router(birds_router)
 
 
